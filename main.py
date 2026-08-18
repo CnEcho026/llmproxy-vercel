7af824046247409cbdeef5e2ecae765350c0c548
Echo <jiaxinglong78@gmail.com>
2026-08-18 15:26:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
+
 from api.hello import router as hello_router
 from fastapi import FastAPI
-from fastapi.responses import Response
+from fastapi.responses import JSONResponse
 from api.servers.generic import router as generic_router
 from api.servers.gemini import router as gemini_router
 from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
 app.include_router(hello_router, prefix="/hello")
 app.include_router(gemini_router, prefix="/gemini")
-app.include_router(generic_router, prefix="") # put generic last
+app.include_router(generic_router, prefix="")  # put generic last
 
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=[ "X-Experimental-Stream-Data"],  # this is needed for streaming data header to be read by the client
+    expose_headers=["X-Experimental-Stream-Data"],
 )
+
 
 @app.get("/")
 def _root():
-    return {"status": "ok", "message": "LLM Proxy is running"}
+    return JSONResponse(
+        content={
+            "status": "ok",
+            "message": "LLM Proxy is running"
+        }
+    )
